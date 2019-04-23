861c69315cf2c4e33afb848e93cabc920a1b0832
Scott Ferguson <scott.ferguson82@gmail.com>
2019-04-23 14:43:29 -0500

    Little more cleanup...kinda enjoying VS Code + Git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,22 +24,6 @@ const appRoutes: Routes = [
         path        : 'apps',
         loadChildren: './main/apps/apps.module#AppsModule'
     },
-    /*{
-        path        : 'pages',
-        loadChildren: './main/pages/pages.module#PagesModule'
-    },
-    {
-        path        : 'ui',
-        loadChildren: './main/ui/ui.module#UIModule'
-    },
-    {
-        path        : 'documentation',
-        loadChildren: './main/documentation/documentation.module#DocumentationModule'
-    },
-    {
-        path        : 'angular-material-elements',
-        loadChildren: './main/angular-material-elements/angular-material-elements.module#AngularMaterialElementsModule'
-    },*/
     {
         path      : '**',
         redirectTo: 'apps/dashboards/analytics'
